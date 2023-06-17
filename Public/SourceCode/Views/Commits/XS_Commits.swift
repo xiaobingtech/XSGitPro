63ad6722ad7730beaaa685e5573fea63e904d9a1
韩云智 <hanyzjob@163.com>
2023-06-17 15:56:34 +0800

    保存变动

---
+++ Public/SourceCode/Views/Commits/XS_Commits.swift
@@ -0,0 +1,8 @@
+//
+//  XS_Commits.swift
+//  XSGitPro
+//
+//  Created by 韩云智 on 2023/6/17.
+//
+
+import Foundation
---
+++ Public/SourceCode/Views/Commits/XS_CommitsView.swift
@@ -0,0 +1,22 @@
+//
+//  XS_CommitsView.swift
+//  XSGitPro
+//
+//  Created by 韩云智 on 2023/6/17.
+//
+
+import SwiftUI
+
+struct XS_CommitsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct XS_CommitsView_Previews: PreviewProvider {
+    static var previews: some View {
+        XS_CommitsView()
+    }
+}
+#endif
